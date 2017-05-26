297611b9e044dca99d5ba836092aea280288414c
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-05-26 10:58:23 -0500

    Write new subdocument tests

--- users1/test/subdocument_test.js
+++ users1/test/subdocument_test.js
@@ -16,4 +16,44 @@ describe('Subdocuments', () => {
         done();
       })
   });
+
+  it('can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        //Must return this promise in order to chain on another .then()
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      })
+  });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        user.posts[0].remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      })
+  });
+
 });
