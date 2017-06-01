dc39afe0cba3c1b5d9ffd4201b9543c598b6104f
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-01 15:25:04 -0500

    Re-init app

---
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -0,0 +1,25 @@
+const Artist = require('../models/artist');
+
+/**
+ * Finds the lowest and highest yearsActive of artists in the Artist collection
+ * @return {promise} A promise that resolves with an object
+ * containing the min and max yearsActive, like { min: 0, max: 14 }.
+ */
+ module.exports = () => {
+   const min = Artist
+     .find({})
+     .sort({ yearsActive: 1 })
+     .limit(1)
+     .then(artist => artist[0].yearsActive);
+
+   const max = Artist
+     .find({})
+     .sort({ yearsActive: -1 })
+     .limit(1)
+     .then(artist => artist[0].yearsActive);
+
+   return Promise.all([min, max])
+     .then(result => {
+       return { min: result[0], max: result[1] };
+     });
+ };
