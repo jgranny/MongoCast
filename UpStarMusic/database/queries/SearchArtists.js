f3563af9a04c63379fa37fa178858f892c03cd3e
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-01 22:41:19 -0500

    Write searchArtists query

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,44 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const buildQuery = (criteria) => {
+    const query = {};
+
+    if (criteria.name) {
+      query.$text = { $search: criteria.name };
+    }
+
+    if (criteria.age) {
+      query.age = {
+        $gte: criteria.age.min,
+        $lte: criteria.age.max
+      };
+    }
+
+    if (criteria.yearsActive) {
+      query.yearsActive = {
+        $gte: criteria.age.min,
+        $lte: criteria.age.max
+      };
+    }
+
+    return query;
+  }
+
+  const query = Artist.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit
+      };
+    });
+
+
 };
