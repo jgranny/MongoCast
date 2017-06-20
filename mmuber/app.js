dd1943ac7d1f49916a08724a502de1a40da19dd8
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-20 17:02:33 -0500

    Create drivers controller test

--- mmuber/app.js
+++ mmuber/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//Invoke json
+//The bodyParser call must be above the routes call
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
--- mmuber/routes/routes.js
+++ mmuber/routes/routes.js
@@ -1,5 +1,8 @@
 const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
+  //Do not invoke these functions, they would run immediately
   app.get('/api', DriversController.greeting);
+
+  app.post('/api/drivers', DriversController.create);
 };
