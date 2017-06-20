f949287b4c7480b20ab390d2f2b0ccf079824984
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-20 10:15:01 -0500

    Fix retire bug and search result count

--- UpStarMusic/database/queries/SetRetired.js
+++ UpStarMusic/database/queries/SetRetired.js
@@ -8,7 +8,7 @@ const Artist = require('../models/artist');
 module.exports = (_ids) => {
   return Artist.update(
     { _id: { $in: _ids } },
-    { retired: false },
+    { retired: true },
     { multi: true }
   );
 };
