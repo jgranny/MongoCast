f87547fa73d4e1b3253893ae393ec672f4ea6054
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-21 10:29:15 -0500

    Add create functionality

--- mmuber/app.js
+++ mmuber/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
+
+//Calling promise fixes deprecation warning
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/mmuber');
 
 //Invoke json
 //The bodyParser call must be above the routes call
--- mmuber/test/controllers/drivers_controller_test.js
+++ mmuber/test/controllers/drivers_controller_test.js
@@ -1,16 +1,27 @@
 const assert = require('assert');
 const request = require('supertest');
+const mongoose = require('mongoose');
 const app = require('../../app');
+
+//This is a work around because mongoose mocha and express don't work well together
+// mocha will try and call controllers multiple times
+const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
 
   it('Post to /api/drivers creates a new driver', done => {
-    request(app)
+    Driver.count().then(count => {
+      request(app)
       .post('/api/drivers')
       .send({ email: 'test@test.com' })
       .end(() => {
-        done();
+
+        Driver.count().then(newCount => {
+          assert(count + 1 === newCount);
+          done();
+        });
       });
+    });
   });
 
 })
